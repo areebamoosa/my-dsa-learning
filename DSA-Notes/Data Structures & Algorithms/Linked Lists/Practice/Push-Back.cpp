53f67f9cff1b193c97d79ed121ff0ec5ee98a19b
Areeba Moosa <areebamoosa153@gmail.com>
2025-09-14 21:05:00 +0500

    Practiced Push front & Push back function on singly linkedlist

---
+++ DSA-Notes/Data Structures & Algorithms/Linked Lists/Practice/Push-Back.cpp
@@ -0,0 +1,62 @@
+// Q2 : Create a function that inserts a new node at the end of a singly linked list. Insert the values 10, 20, 30 one by one using this function, then print the list.
+
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *next;
+
+    Node(int d){
+        data = d;
+        next = NULL;
+    }
+};
+
+class linkedList{
+    Node* head;
+    Node* tail;
+
+    public:
+
+    linkedList(){
+        head = tail = NULL;
+    }
+
+    // Push back function
+    void push_back(int data){
+        Node *newNode = new Node(data);
+
+        if (head == NULL){
+            head = tail = newNode;
+            return;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+            tail->next = NULL;
+        }
+    }
+
+    // Printing ll
+    void printLL(){
+        Node *temp = head;
+        while(temp != NULL){
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+int main(){
+
+    linkedList ll;
+    ll.push_back(10);
+    ll.push_back(20);
+    ll.push_back(30);
+
+    ll.printLL();
+
+    return 0;
+}
---
+++ DSA-Notes/Data Structures & Algorithms/Linked Lists/Practice/Push-Front.cpp
@@ -0,0 +1,65 @@
+// Create a function that inserts a new node at the beginning of a singly linked list. Insert the values 10, 20, 30 one by one using this function, then print the list.
+
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+
+    int data;
+    Node* next;
+
+    Node(int d){
+        data = d;
+        next = NULL;
+    }
+};
+
+class linkedList{
+
+    Node *head;
+    Node *tail;
+
+    public:
+
+    linkedList(){
+        head = tail = NULL;
+    }
+
+    // Push Front function
+    void push_front(int data){
+        Node *newNode = new Node(data);
+
+        if (head == NULL){
+            head = tail = newNode;
+            return;
+        } else {
+        newNode->next = head;
+        head = newNode;
+        }
+
+    }
+
+    // Printing Linkedlist
+    void printLL(){
+        Node* temp = head;
+
+        while (temp != NULL){
+            cout << temp -> data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+int main(){
+    linkedList ll;
+
+    ll.push_front(10);
+    ll.push_front(20);
+    ll.push_front(30);
+
+    ll.printLL();
+
+    return 0;
+}
