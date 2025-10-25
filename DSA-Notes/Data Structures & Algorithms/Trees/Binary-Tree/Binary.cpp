626e74e576a25a15dc847476d4775c2b5b3a86cd
Areeba Moosa <areebamoosa153@gmail.com>
2025-10-25 16:42:53 +0500

    Practiced Preorder, Postorder,Inorder & Levelorder Traversal in Binary Trees

--- DSA-Notes/Data Structures & Algorithms/Trees/Binary-Tree/Binary.cpp
+++ DSA-Notes/Data Structures & Algorithms/Trees/Binary-Tree/Binary.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 using namespace std;
 
 class Node{
@@ -32,15 +33,92 @@ Node* buildTree(vector<int> preorder){
 
 }
 
+// Pre Order Traversal
+void preOrder(Node* root){
+    if ( root == NULL){
+        return;
+    }
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+// InOrder Traversal
+void inOrder(Node* root){
+    if ( root == NULL){
+        return;
+    }
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+
+// Post Order Traversal
+void postOrder(Node* root){
+    if ( root == NULL){
+        return;
+    }
+    postOrder(root->left);
+    postOrder(root->right);
+    cout << root->data << " ";
+}
+
+// Level Order Traversal
+void levelOrder(Node* root){
+    queue<Node*> q;
+
+    q.push(root);
+    q.push(NULL); 
+
+    while(q.size() > 0){
+        Node* curr = q.front();
+        q.pop();
+
+        if ( curr == NULL){
+            if(!q.empty()){
+                cout << endl;
+                q.push(NULL);
+                continue;
+            } else {
+                break;
+            }
+        }
+
+        cout << curr->data << " ";
+
+        if (curr->left != NULL){
+            q.push(curr->left);
+        }
+        if (curr->right != NULL){
+            q.push(curr->right);
+        }
+    }
+
+    cout << endl;
+}
+
 int main(){
 
     vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
 
     Node* root = buildTree(preorder);
 
-    cout << root->data << endl; 
-    cout << root->left->data << endl; 
-    cout << root->right->data << endl; 
+    cout << "Pre Order Traversal"<< endl;
+
+    preOrder(root);
+    cout << endl;
+
+    cout << "In Order Traversal"<< endl;
+    inOrder(root);
+    cout << endl;
+
+    cout << "Post Order Traversal"<< endl;
+    postOrder(root);
+    cout << endl;
+
+    cout << "Level Order Traversal"<< endl;
+    levelOrder(root);
+    cout << endl;
 
     return 0;
 }
