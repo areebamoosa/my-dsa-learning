d686a42873dd8ee2d5b6162775506843bbd7678e
Areeba Moosa <areebamoosa153@gmail.com>
2025-11-04 17:34:46 +0500

    Understood & built a Binary Search Tree

---
+++ DSA-Notes/Data Structures & Algorithms/Trees/Binary-Tree/BinarySearch.cpp
@@ -0,0 +1,63 @@
+// Build a Binary Search Tree
+
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Inserting nodes in tree
+Node* insert(Node* root, int val){
+    if (root == NULL){
+        return new Node (val);
+    }
+
+    if ( val < root->data){
+        root->left = insert(root->left, val);
+    }
+    else{
+        root->right = insert(root->right, val);
+    }
+
+    return root;
+}
+
+Node* buildBST(vector<int> arr){
+
+    Node* root = NULL;
+
+    for (int val : arr){
+        root = insert(root , val);
+    }
+    return root;
+}
+
+void inOrder(Node* root){
+    if ( root == NULL){
+        return;
+    }
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+
+int main(){
+
+    vector<int> arr = {3,2,1,5,6,4};
+
+    Node* root = buildBST(arr);
+
+    inOrder(root);
+    cout << endl;
+
+    return 0;
+}
